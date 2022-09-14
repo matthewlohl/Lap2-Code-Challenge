eddeca53aba571fdefc3834e683f568c500a6dfa
Camila Pessoa <caape13@hotmail.com>
2022-09-14 12:19:39 +0100

    implement index, show, create and destroy Controllers

---
+++ api/controllers/posts.js
@@ -0,0 +1,44 @@
+const Post = require('../models/Post')
+
+async function index (req, res) {
+
+  try {
+    const posts = await Post.all;
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json({err})
+  }
+
+}
+
+async function show (req, res) {
+
+  try {
+    const post = await Post.findById(parseInt(req.params.id));
+    res.status(200).json(dog)
+
+  } catch (err) {
+    res.status(404).json({err})
+  }
+}
+
+async function create (req, res) {
+
+  try {
+    const post = await Post.create(req.body);
+    res.send(201).json(post)
+  } catch (err) {
+    res.status(422).json({err})
+  }
+}
+
+async function destroy (req, res) {
+
+  try {
+    const post = await Post.findById(parseInt(req.params.id));
+    const resp = await post.destroy;
+    res.status(204).end()
+  } catch (err) {
+    res.status(404).json({err})
+  }
+}
